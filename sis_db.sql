d4326e9b7a787879cad67c0d84f97e1b48b55437
Sanchita Devkar <98245246+SanchitaDevkar@users.noreply.github.com>
2025-04-11 10:50:36 +0530

    Add files via upload

---
+++ sis_db.sql
@@ -0,0 +1,67 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Apr 11, 2025 at 07:19 AM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.0.30
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `sis_db`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `courses`
+--
+-- Error reading structure for table sis_db.courses: #1932 - Table 'sis_db.courses' doesn't exist in engine
+-- Error reading data for table sis_db.courses: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `sis_db`.`courses`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `enrollments`
+--
+-- Error reading structure for table sis_db.enrollments: #1932 - Table 'sis_db.enrollments' doesn't exist in engine
+-- Error reading data for table sis_db.enrollments: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `sis_db`.`enrollments`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `payments`
+--
+-- Error reading structure for table sis_db.payments: #1932 - Table 'sis_db.payments' doesn't exist in engine
+-- Error reading data for table sis_db.payments: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `sis_db`.`payments`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `students`
+--
+-- Error reading structure for table sis_db.students: #1932 - Table 'sis_db.students' doesn't exist in engine
+-- Error reading data for table sis_db.students: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `sis_db`.`students`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `teachers`
+--
+-- Error reading structure for table sis_db.teachers: #1932 - Table 'sis_db.teachers' doesn't exist in engine
+-- Error reading data for table sis_db.teachers: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `sis_db`.`teachers`' at line 1
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
